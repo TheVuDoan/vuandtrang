d2f8873e4cdcace879dbddad6ff24d6416b5ca19
The Vu <thevudoan@gmail.com>
2023-02-24 14:42:31 +0700

    WED-7 Add ScrollToTop Button (#9)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
 import Footer from './Footer';
-import SoundPlayer from '../components/SoundPlayer';
+import SoundPlayer from './SoundPlayer';
+import ScrollToTop from './ScrollToTop';
 
 const Layout = ({ children }) => {
   return (
     <>
       <main>{children}</main>
       <SoundPlayer />
+      <ScrollToTop />
       <Footer />
     </>
   );
--- pages/album/index.tsx
+++ pages/album/index.tsx
@@ -11,16 +11,16 @@ const Album: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>VuandTrang Wedding Site!</title>
+        <title>VuandTrang Wedding Album!</title>
         <meta
           property="og:image"
           content="https://vandt.day/hero.webp"
         />
       </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
+      <div className="mx-auto max-w-[1960px] p-4">
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0 bg-[url('../public/common/orangeBackground.webp')]">
-            <img className="absolute -top-8 md:w-3/4" src={logo.src} />
+            <Image className="absolute -top-8 md:w-3/4" src={logo.src} alt="logo" width={358} height={358} />
             <h1 className="mt-8 text-5xl tracking-widest font-great-vibes text-gray-700">
               Vũ & Trang
             </h1>
@@ -58,7 +58,7 @@ const Album: NextPage = ({ images }: { images: ImageProps[] }) => {
             </div>
           ))}
         </div>
-      </main>
+      </div>
     </>
   )
 }
